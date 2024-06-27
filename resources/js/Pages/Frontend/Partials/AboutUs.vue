5e0007b58b423782c0a49233ba62a6b5d8346a1f
ramdhannassyirahh <ramdhanassyirahh@gmail.com>
2024-06-27 18:57:08 +0700

    feat: Landing-Page

--- resources/js/Pages/Frontend/Partials/AboutUs.vue
+++ resources/js/Pages/Frontend/Partials/AboutUs.vue
@@ -4,7 +4,7 @@
             <div class="grid md:grid-cols-2 items-center justify-items-center gap-6 px-6 pt-6 ">
                 <div class="space-y-4">
                     <p>About Us</p>
-                    <h1>The Base Place <br/>To Audit Your Software </h1>
+                    <h1 class="md:text-5xl font-bold text-3xl">The Base Place <br/>To Audit Your Software </h1>
                     <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
                     <div>
                         <Button class="bg-black px-6 py-2 rounded-lg text-white">
---
+++ resources/js/Pages/Frontend/Partials/Testimonials.vue
@@ -0,0 +1,41 @@
+<template lang="">
+    <div>
+        <div class="testi">
+            <div class="space-y-6 pt-6 px-4">
+                <div class="space-y-4">
+                    <p>Testimonials</p>
+                    <h1 class="md:text-5xl font-bold text-3xl">What Our Clients Say <br/> About Us </h1>
+                </div>
+                <div>
+                    <div class="overflow-x-auto no-scrollbar">
+                        <div class="">
+                            <div class="inline-flex gap-4">
+                                <CardProduct />
+                                <CardProduct/>
+                                <CardProduct/>
+                                <CardProduct/>
+                                <CardProduct/>
+                                <CardProduct/>
+                                <CardProduct/>
+                                <CardProduct/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import CardProduct from '@/Components/Card/CardProduct.vue';
+export default {
+
+    components: {
+        CardProduct
+    },
+
+}
+</script>
+<style lang="">
+
+</style>
